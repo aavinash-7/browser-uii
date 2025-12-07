5e8457ec45066707d8635de84ce0e0aae30ada77
aavinash_7 <aajava20@gmail.com>
2025-12-07 21:20:43 +0530

    Fix: Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/browser-uii/", // <-- must match your repo name!!
-});
+  base: "/browser-uii/", // 👈 your repo name EXACTLY
+})
